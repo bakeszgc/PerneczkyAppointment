98c51f98aff74633e256016b347963b6a2ef5425
Bence Bacsa <bacsabence00@gmail.com>
2025-03-17 19:31:00 +0100

    soft deletes for appointments

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -6,11 +6,12 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Appointment extends Model
 {
     /** @use HasFactory<\Database\Factories\AppointmentFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['barber_id','service_id','user_id','app_start_time','app_end_time','comment','price'];
 

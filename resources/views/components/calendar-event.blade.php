f807260bdac207da4931b504f41bedfdc2c0c39a
Bence Bacsa <bacsabence00@gmail.com>
2025-04-10 14:34:51 +0200

    time off colour fix

--- resources/views/components/calendar-event.blade.php
+++ resources/views/components/calendar-event.blade.php
@@ -6,8 +6,8 @@ height: calc(" . \Carbon\Carbon::parse($appointment->app_start_time)->diffInMinu
 
     <a href="{{ $appointment->service_id !== 1 ? route('appointments.show',$appointment) : route('time-off.show',$appointment) }}">
         <div @class([
-            'bg-blue-100 hover:bg-blue-200 border border-blue-300 transition-all text-blue-600 h-full px-1 max-sm:px-0.5 rounded-md max-lg:translate-y-6 overflow-hidden max-sm:text-xs' => true,
-            'border-dashed bg-green-100 hover:bg-green-200 border-green-600 text-green-600' => $appointment->service_id === 1,
+            'bg-blue-100 hover:bg-blue-200 border border-blue-300 transition-all text-blue-600 h-full px-1 max-sm:px-0.5 rounded-md max-lg:translate-y-6 overflow-hidden max-sm:text-xs',
+            'border-dashed bg-green-100 hover:bg-green-200 border-green-600 text-green-600' => $appointment->service_id == 1,
             'bg-slate-100 hover:bg-slate-200 text-slate-600 border-slate-300' => $appointment->app_start_time < now(),
             ])>
 

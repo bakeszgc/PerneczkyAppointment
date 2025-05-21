483b6830ca0180799875b44151ff918a540e7681
Bence Bacsa <bacsabence00@gmail.com>
2025-05-21 15:22:35 +0200

    showcard component

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -85,22 +85,8 @@
         </form>
     </x-card>
 
-    <x-card x-data="{open: {{ $showPassword ? 'true' : 'false' }}}" class="mb-4">
-        <div class="flex justify-between items-center cursor-pointer transition-all" @click="open = !open">
-            <h2 class="text-xl font-bold flex gap-2 items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
-                </svg>
-                <span>Change your password</span>
-            </h2>
-            <div class="hover:bg-blue-100 transition-all rounded-full p-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" x-bind:class="!open ? 'size-6 transition-all' : 'rotate-180 size-6 transition-all'">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"/>
-                </svg>
-            </div>
-        </div>
-
-        <form action="{{ route('users.update-password',auth()->user()->id) }}" method="POST" x-show="open" x-transition>
+    <x-show-card :show="$showPassword" type="password" class="mb-4">
+        <form action="{{ route('users.update-password',auth()->user()->id) }}" method="POST" >
             @csrf
             @method('PUT')
             <div class="flex gap-4 mt-4 mb-4">
@@ -149,6 +135,5 @@
             </div>
             <x-button role="loginMain" :full="true">Change Password</x-button>
         </form>
-
-    </x-card>
+    </x-show-card>
 </x-user-layout>

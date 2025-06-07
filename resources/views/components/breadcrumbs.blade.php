f7b59afbbf91e6b2beb90b740b9d899e2ea6344c
Bence Bacsa <bacsabence00@gmail.com>
2025-06-07 15:40:19 +0200

    home link no longer redirects to baddadan

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <a href="https://youtu.be/rkjNL4dX-U4?t=64" class="hover:underline">Home</a>
+    <a href="{{ route('home') }}" class="hover:underline">Home</a>
     @foreach ($links as $linkDisplay => $link)
         @if ($linkDisplay !== 'All')
             ➡️

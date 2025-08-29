cc6f45d0831b7faf88e26f01a8c757c11efd6194
Bence Bacsa <bacsabence00@gmail.com>
2025-08-29 17:04:10 +0200

    time off card fix

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,5 +1,5 @@
 <select name="{{ $name }}" id="{{ $id }}" {{ $disabled ? 'disabled' : '' }} {{ $attributes->merge(['class' => 'border border-slate-300 rounded-md transition-all 
 hover:border-blue-500 hover:shadow-md
-disabled:bg-slate-100 disabled:text-slate-400 disabled:hover:border-slate-300 disabled:hover:drop-shadow-none']) }}>
+disabled:bg-slate-100 disabled:text-slate-400 disabled:hover:border-slate-300 disabled:hover:shadow-none']) }}>
     {{ $slot }}
 </select>
--- resources/views/components/time-off-card.blade.php
+++ resources/views/components/time-off-card.blade.php
@@ -1,8 +1,26 @@
+@php
+    use Carbon\Carbon;
+
+    switch ($access) {
+        case 'barber':
+            $showRoute = route('time-offs.show',$appointment);
+            $editRoute = route('time-offs.edit',$appointment);
+            $destroyRoute = route('time-offs.destroy',$appointment);
+        break;
+
+        case 'admin':
+            $showRoute = route('admin-time-offs.show',$appointment);
+            $editRoute = route('admin-time-offs.edit',$appointment);
+            $destroyRoute = route('admin-time-offs.destroy',$appointment);
+        break;
+    }
+@endphp
+
 <x-card {{$attributes->merge(['class' => ''])}}>
     <div @class(['flex justify-between' => true, 'text-slate-500' => $appointment->deleted_at || $appointment->app_start_time < now()])>
         <div>
             <h2 @class(['font-bold text-2xl max-sm:text-lg mb-1 flex items-center gap-2' => true, 'text-green-600 hover:text-green-800' => $appointment->app_start_time >= now() && !$appointment->deleted_at ])>
-                <a href="{{ route('time-offs.show',$appointment) }}"
+                <a href="{{ $showRoute }}"
                 @class(['line-through' => $appointment->deleted_at])>
                     {{$appointment->user->first_name . " " . $appointment->user->last_name}} #{{$appointment->id}}
                 </a>
@@ -14,35 +32,35 @@
         </div>
         <div class="text-right">
             <h2 class="font-bold text-2xl max-sm:text-lg mb-1">
-                @if (Carbon\Carbon::parse($appointment->app_start_time)->isToday())
+                @if (Carbon::parse($appointment->app_start_time)->isToday())
                     Today
-                @elseif (Carbon\Carbon::parse($appointment->app_start_time)->isTomorrow())
+                @elseif (Carbon::parse($appointment->app_start_time)->isTomorrow())
                     Tomorrow
-                @elseif (Carbon\Carbon::parse($appointment->app_start_time)->isYesterday())
+                @elseif (Carbon::parse($appointment->app_start_time)->isYesterday())
                     Yesterday
                 @else
-                    {{ Carbon\Carbon::parse($appointment->app_start_time)->format('Y.m.d.') }}
+                    {{ Carbon::parse($appointment->app_start_time)->format('Y.m.d.') }}
                 @endif
             </h2>
             <h3 class="font-medium text-lg max-sm:text-sm">
-                {{ Carbon\Carbon::parse($appointment->app_start_time)->format('G:i') . ' - ' . Carbon\Carbon::parse($appointment->app_end_time)->format('G:i') }}
+                {{ Carbon::parse($appointment->app_start_time)->format('G:i') . ' - ' . Carbon::parse($appointment->app_end_time)->format('G:i') }}
             </h3>
             <h4 class="text-base max-sm:text-sm">
                 {{ $appointment->getDuration() >= 600 ? 'Full day' : $appointment->getDuration() . ' minutes' }}
             </h4>
         </div>
     </div>
-    <div class="flex gap-2 mt-4">
+    <div class="flex gap-2">
         @if ($showDetails)
-            <x-link-button :link="route('time-offs.show',$appointment)" role="show">
+            <x-link-button :link="$showRoute" role="show">
                 Details
             </x-link-button>
         @endif
         @if ($appointment->app_start_time >= now('Europe/Budapest') && !$appointment->deleted_at)
-            <x-link-button :link="route('time-offs.edit',$appointment)" role="edit">
+            <x-link-button :link="$editRoute" role="edit">
                 Edit
             </x-link-button>
-            <form action="{{route('time-offs.destroy',$appointment) }}" method="POST">
+            <form action="{{ $destroyRoute }}" method="POST">
                 @csrf
                 @method('DELETE')
                 <x-button role="destroy">

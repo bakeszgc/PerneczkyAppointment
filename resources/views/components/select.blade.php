c250be788f7a5bd4d03acbc4d4f31fe12a60fed8
Bence Bacsa <bacsabence00@gmail.com>
2025-08-29 11:25:31 +0200

    select component shadow fix

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,3 +1,5 @@
-<select name="{{ $name }}" id="{{ $id }}" {{ $disabled ? 'disabled' : '' }} {{ $attributes->merge(['class' => 'border border-slate-300 rounded-md hover:border-blue-500 hover:drop-shadow transition-all disabled:bg-slate-100 disabled:text-slate-400 disabled:hover:border-slate-300 disabled:hover:drop-shadow-none']) }}>
+<select name="{{ $name }}" id="{{ $id }}" {{ $disabled ? 'disabled' : '' }} {{ $attributes->merge(['class' => 'border border-slate-300 rounded-md transition-all 
+hover:border-blue-500 hover:shadow-md
+disabled:bg-slate-100 disabled:text-slate-400 disabled:hover:border-slate-300 disabled:hover:drop-shadow-none']) }}>
     {{ $slot }}
 </select>

0ad4758584a58dc9bbdbf69cebd07805e52944ec
Bence Bacsa <bacsabence00@gmail.com>
2025-10-07 09:28:46 +0200

    sentence casing for email verification mail

--- resources/views/emails/email_verification.blade.php
+++ resources/views/emails/email_verification.blade.php
@@ -4,7 +4,7 @@
     <p class="mb-8">Welcome to PERNECZKY BarberShop, thank you for creating an account! Before booking your first appointment please verify your email address by clicking the button below:</p>
 
     <div class="text-center mb-8">
-        <a href="{{ $url }}" id="ctaButton" target="_blank">Verify Email Address</a>
+        <a href="{{ $url }}" id="ctaButton" target="_blank">Verify email address</a>
     </div>
 
     <p class="mb-8">If you did not create an account, no further action is required from you. If you have any questions or require assistance, feel free to contact us at <a href="mailto:info@perneczkybarbershop.hu" class="link">info@perneczkybarbershop.hu</a> or call us at <a href="tel:+36704056079" class="link">+36 70 405 6079</a>.</p>
@@ -12,6 +12,6 @@
     <hr class="mb-8" />
 
     <p id="linkTrouble">
-        If you're having trouble clicking the "Verify Email Address" button, copy and paste the URL below into your web browser: <a href="{{ $url }}" class="link word-break">{{ $url }}</a>
+        If you're having trouble clicking the "Verify email address" button, copy and paste the URL below into your web browser: <a href="{{ $url }}" class="link word-break">{{ $url }}</a>
     </p>
 </x-email-layout>

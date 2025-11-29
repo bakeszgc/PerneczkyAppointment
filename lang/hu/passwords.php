f457508d6cc3e396c7ad5cdd295f131401d0955b
Bence Bacsa <bacsabence00@gmail.com>
2025-11-29 22:39:40 +0100

    localization for passwords messages

---
+++ lang/hu/passwords.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'reset' => 'A jelszavad sikeresen vissza lett állítva!',
+    'sent' => 'Elküldtük neked a jelszó-visszaállítási linket e-mailben.',
+    'throttled' => 'Kérlek várj, mielőtt újra próbálkoznál!',
+    'token' => 'Ez a jelszó-visszaállító token érvénytelen.',
+    'user' => "Nem találunk felhasználót ezzel az e-mail címmel.",
+
+];

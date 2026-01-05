c334026a57c4856df64670b5b6a4d94e7b967ac4
Bence Bacsa <bacsabence00@gmail.com>
2026-01-05 09:43:04 +0100

    timeoff not appearing on admin page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,7 +13,7 @@ class AdminController extends Controller
     public function index() {
         $barbers = Barber::limit(6)->get();
 
-        $services = Service::where('is_visible','=',1)->withCount(['appointments as appointments_count' => function ($q) {
+        $services = Service::withoutTimeoff()->where('is_visible','=',1)->withCount(['appointments as appointments_count' => function ($q) {
             $q->withoutTrashed();
         }])->orderByDesc('appointments_count')->limit(5)->get();
 
